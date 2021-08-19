28346efcd79d8b8cb55b82311d61b38da528fa7e
Lazer666 <v6572000@gmail.com>
2021-08-19 21:12:47 +0800

    adjust code that can lauch the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,10 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField]private Rigidbody2D ball_rigi;
+    [SerializeField]private SpringJoint2D ball_sprjt;
+    [SerializeField]private float detach_time;
     private Camera camera_main;
+    private bool dragging;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +20,38 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(ball_rigi == null)
+        {
+            return;
+        }
         if(!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            ball_rigi.isKinematic = false;
+            if(dragging)
+            {
+                Launch_Ball();
+            }
             return;
         }
         else
         {
+            dragging = true;
             Vector2 touch_pos = Touchscreen.current.primaryTouch.position.ReadValue();
             Vector3 world_pos = camera_main.ScreenToWorldPoint(touch_pos);
             ball_rigi.position = world_pos;
             ball_rigi.isKinematic = true;
         }
+    }
+
+    private void Launch_Ball()
+    {
+        ball_rigi.isKinematic = false;
+        ball_rigi = null;
+        // Invoke("Detach_Ball",detach_time); 與下一行同義
+        Invoke(nameof(Detach_Ball), detach_time);
+    }
+    public void Detach_Ball()
+    {
+        ball_sprjt.enabled = false;
+        ball_sprjt = null;
     }
 }
